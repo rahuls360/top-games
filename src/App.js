a8491f52ba2279be9a0495d8e77e5f8825b9b244
Rahul Makhija <rahuls360@gmail.com>
2019-05-31 22:45:44 +0530

    display data using pagination

--- src/App.js
+++ src/App.js
@@ -22,11 +22,58 @@ class App extends Component {
   }
 
   handleUpdate = result => {
+    console.log(result);
     this.setState({ data: result.data });
   };
 
+  getColumns = () => {
+    return [
+      {
+        title: "Rank",
+        dataIndex: "Rank",
+        key: "rank"
+      },
+      {
+        title: "Name",
+        dataIndex: "name",
+        key: "name"
+      },
+      {
+        title: "Platform",
+        dataIndex: "Platform",
+        key: "Platform"
+      },
+      {
+        title: "Year",
+        dataIndex: "Year",
+        key: "Year"
+      },
+      {
+        title: "Genre",
+        dataIndex: "Genre",
+        key: "Genre"
+      },
+      {
+        title: "Publisher",
+        dataIndex: "Publisher",
+        key: "Publisher"
+      },
+      {
+        title: "Global_Sales",
+        dataIndex: "Global_Sales",
+        key: "Global_Sales"
+      }
+    ];
+  };
+
   render() {
-    return <div>Welcome to the Application</div>;
+    return (
+      <>
+        <div>Welcome to the Application</div>
+
+        <Table dataSource={this.state.data} columns={this.getColumns()} />
+      </>
+    );
   }
 }
 
